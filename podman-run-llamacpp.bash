c12ed6967656de1558178050944efc7d90a1224c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-07-15 23:01:13 +0200

    Update podman-run-llamacpp.bash

--- podman-run-llamacpp.bash
+++ podman-run-llamacpp.bash
@@ -1,2 +1,3 @@
 podman build --no-cache --rm --file Containerfile --tag llamacpp:demo .
-podman run --interactive --tty --publish 8080:8080 llamacpp:demo
+#podman run --interactive --tty --publish 8080:8080 llamacpp:demo
+podman run --interactive --tty --publish 8080:8080 --device nvidia.com/gpu=all --security-opt=label=disable llamacpp:demo
