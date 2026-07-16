f53960cc3bc4afac217d785e0cc1003a9ceb1e82
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-07-16 06:54:45 +0200

    Update podman-run-llamacpp.bash

--- podman-run-llamacpp.bash
+++ podman-run-llamacpp.bash
@@ -1,3 +1,4 @@
 podman build --no-cache --rm --file Containerfile --tag llamacpp:demo .
 #podman run --interactive --tty --publish 8080:8080 llamacpp:demo
-podman run --interactive --tty --publish 8080:8080 --device nvidia.com/gpu=all --security-opt=label=disable llamacpp:demo
+podman run --interactive --tty --publish 8080:8080 --device nvidia.com/gpu=all --security-opt=label=disable llamacpp:demo # NVIDIA
+#podman run --interactive --tty --publish 8080:8080 --device /dev/kfd --device /dev/dri --group-add keep-groups --security-opt=label=disable llamacpp:demo # AMD
